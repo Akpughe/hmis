cc364674bbd7067ea8e014c6dbc6dd2acfc7f5b4
“Akpughe” <davidakpughe2@gmail.com>
2021-02-13 16:20:45 +0100

    loading user

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,20 +1,38 @@
 import React, { useEffect } from 'react';
-import axios from 'axios'
+import axios from 'axios';
 import 'antd/dist/antd.css';
 import '../styles/globals.css';
 import '../styles/tailwind.css';
 // redux
-import {withRedux, createWrapper} from 'next-redux-wrapper';
+import { withRedux, createWrapper } from 'next-redux-wrapper';
 import { Provider } from 'react-redux';
-import { withRouter } from 'next/router';
+import { withRouter, useRouter } from 'next/router';
 import store from '../store';
 import setAuthToken from '../utils/setAuthToken';
-// if (localStorage.token) {
-//   setAuthToken(localStorage.token);
-// }
-axios.defaults.baseURL = "http://localhost:4000"
+import { loadUser } from '../actions/auth';
 
-function MyApp({ Component, pageProps }) {
+axios.defaults.baseURL = 'http://localhost:4000';
+
+function MyApp({ Component, pageProps, isAuthenticated}) {
+  const router = useRouter();
+
+  useEffect(() => {
+    store.dispatch(loadUser());
+
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+
+    if (!isAuthenticated) {
+          return router.push('/');
+        }
+  }, []);
+
+  // useEffect(() => {
+  //   if (isAuthenticated) {
+  //     return router.push('/dashboard');
+  //   }
+  // });
   return (
     <Provider store={store}>
       <Component {...pageProps} />
@@ -22,7 +40,7 @@ function MyApp({ Component, pageProps }) {
   );
 }
 const makeStore = () => store;
-const wrapper = createWrapper(makeStore)
+const wrapper = createWrapper(makeStore);
 
-export default wrapper.withRedux(MyApp)
+export default wrapper.withRedux(MyApp);
 // export default withRedux(makeStore)(MyApp);
