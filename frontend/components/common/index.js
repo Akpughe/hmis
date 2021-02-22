addcae1d4f6b60583748dd4048bd8a584e221e09
“Akpughe” <davidakpughe2@gmail.com>
2021-02-22 16:56:17 +0100

    general commit

--- frontend/components/common/index.js
+++ frontend/components/common/index.js
@@ -4,6 +4,30 @@ import Card from './Card';
 import Banner from './Banner';
 import Chart from './Chart';
 import Table from './Table';
-import {newPatient, month} from '../../data/hospitaldata'
+import PatientRegisterForm from './PatientRegisterForm';
+import PatientView from './PatientView';
+import {
+  newPatient,
+  month,
+  stateOfOrigin,
+  nationality,
+  columns,
+  data,
+} from '../../data/hospitaldata';
 
-export { MainLayout, Navbrand, Card, Banner, Chart, Table, newPatient, month };
+export {
+  MainLayout,
+  Navbrand,
+  Card,
+  Banner,
+  Chart,
+  Table,
+  newPatient,
+  month,
+  stateOfOrigin,
+  nationality,
+  columns,
+  data,
+  PatientRegisterForm,
+  PatientView,
+};
