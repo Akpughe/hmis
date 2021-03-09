a76acd23b735ae5fc680a44e59e95f845a51cf96
“Akpughe” <davidakpughe2@gmail.comgit config --global user.email davidakpughe2@gmail.com>
2021-03-09 21:01:04 +0100

    populate appointment with user details and get appointment by id

--- controllers/Appointment.js
+++ controllers/Appointment.js
@@ -21,7 +21,7 @@ exports.getAllAppoinments = async (req, res, next) => {
         errors: [{ msg: 'You do not have permission to perform this action' }],
       });
 
-    const appointments = await Appointment.find();
+    const appointments = await Appointment.find().populate('user',['lastname', 'firstname']);
 
     res.status(200).json(appointments);
   } catch (err) {
@@ -30,7 +30,17 @@ exports.getAllAppoinments = async (req, res, next) => {
   }
 };
 
-exports.getAppointmentById = async (req, res, next) => {};
+exports.getAppointmentById = async (req, res, next) => {
+  const appointmentId = req.appointmentId;
+  const userId = req.userId;
+  try {
+    const appointment = await Appointment.findById(userId);
+    res.json(appointment);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+};
 
 exports.bookAppointment = async (req, res, next) => {
   const errors = validationResult(req);
@@ -57,7 +67,7 @@ exports.bookAppointment = async (req, res, next) => {
   const patientId = req.patientId;
   try {
     const user = await User.findById(userId);
-    // const patient = await Patient.find();
+    const patient = await Patient.findOne(patientId);
     if (!user)
       return res.status(404).json({ errors: [{ msg: 'account not found' }] });
 
@@ -70,10 +80,12 @@ exports.bookAppointment = async (req, res, next) => {
     });
 
     user.appointment.push(appointment._id);
+
     // patient.appointment.push(appointment._id);
 
     await appointment.save();
     await user.save();
+    await patient.save();
 
     res
       .status(201)
