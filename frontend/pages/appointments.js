30be84e4b8c53872e9c12433cb4545f53468bf92
“Akpughe” <davidakpughe2@gmail.comgit config --global user.email davidakpughe2@gmail.com>
2021-03-09 20:59:23 +0100

    show all appointments

--- frontend/pages/appointments.js
+++ frontend/pages/appointments.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import {Table} from '../components/common'
+import React from 'react';
+import Head from 'next/head';
+import { MainLayout, AppointmentView } from '../components/common';
 
 const appointments = () => {
-    return (
-        <Table>
-            
-        </Table>
-    )
-}
+  return (
+    <>
+      <Head>
+        <title>HMIS | Appointments</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <MainLayout>
+        <div className="flex flex-col flex-grow pl-80 pt-20 mb-10 bg-gray-100 h-full">
+          <h1 className="text-3xl font-normal">Appointments</h1>
+          <div className="mb-8 mt-4 flex bg-white h-full w-10/12 ">
+                <AppointmentView/>
+          </div>
+        </div>
+      </MainLayout>
+    </>
+  );
+};
 
-export default appointments
+export default appointments;
