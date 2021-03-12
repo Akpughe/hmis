b9be3604ab027f41da82588993863f205dce47e8
“Akpughe” <davidakpughe2@gmail.comgit config --global user.email davidakpughe2@gmail.com>
2021-03-12 23:55:24 +0100

    added doctor reducer to index

--- frontend/reducers/index.js
+++ frontend/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import auth from './auth'
 import patient from './patient'
 import appointment from './appointment'
+import doctor from './doctor'
 
 
 export default combineReducers({
     auth,
     patient,
-    appointment
+    appointment,
+    doctor
 })
