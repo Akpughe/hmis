23a95c0b159466e0297ea077d052e1295473d585
“Akpughe” <davidakpughe2@gmail.comgit config --global user.email davidakpughe2@gmail.com>
2021-04-05 16:56:22 +0100

    nprogress

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,8 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import Head from 'next/head'
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import 'antd/dist/antd.css';
 import '../styles/globals.css';
 import '../styles/tailwind.css';
@@ -12,6 +15,19 @@ import setAuthToken from '../utils/setAuthToken';
 import { loadUser } from '../actions/auth';
 
 axios.defaults.baseURL = 'http://localhost:4000';
+
+const showProgressBar = (delay) => {
+  const timer = setTimeout(() => NProgress.start(), delay);
+  Router.events.on('routeChangeComplete', () => stopProgress(timer));
+  Router.events.on('routeChangeError', () => stopProgress(timer));
+};
+
+const stopProgress = (timer) => {
+  clearTimeout(timer);
+  NProgress.done();
+};
+
+Router.events.on('routeChangeStart', () => showProgressBar(500));
 
 function MyApp({ Component, pageProps, isAuthenticated }) {
   const router = useRouter();
@@ -26,6 +42,11 @@ function MyApp({ Component, pageProps, isAuthenticated }) {
 
   return (
     <Provider store={store}>
+      <Head>
+      <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+        <link rel="stylesheet" type="text/css" href="/css/nprogress.css" />
+        <title>Health Pro</title>
+      </Head>
       <Component {...pageProps} />
     </Provider>
   );
