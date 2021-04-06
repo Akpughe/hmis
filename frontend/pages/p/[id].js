dfd6033a0021f95b8baecc2d5996654e3a1877a0
“Akpughe” <davidakpughe2@gmail.comgit config --global user.email davidakpughe2@gmail.com>
2021-04-06 10:13:17 +0100

    patient id page

--- frontend/pages/p/[id].js
+++ frontend/pages/p/[id].js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { MainLayout, EnterVitals } from '../../components/common';
+import { MainLayout, EnterVitals, Diagnosis } from '../../components/common';
 import { createVitalReport } from '../../actions/vitals';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -8,11 +8,11 @@ import { getUsers, getPatientById } from '../../actions/patient';
 import axios from 'axios';
 import Link from 'next/link';
 
-const PatientDetails = ( props) => {
+const PatientDetails = (props) => {
   return (
     <>
       {/* <MainLayout> */}
-      <div className="flex flex-col flex-grow pl-16 pr-8 pt-20 mb-10 bg-gray-100 h-full">
+      <div className="flex flex-col flex-grow pl-16 pr-8 pt-20 mb-10 bg-gray-100 h-screen">
         <Link href="/patients"> Back</Link>
         <div className="flex p-9">
           <div className="flex flex-col items-center w-96 h-auto mr-8  bg-white border rounded-3xl ">
@@ -25,15 +25,15 @@ const PatientDetails = ( props) => {
                 <div className="main_one flex flex-col">
                   <div className="mb-8">
                     <small>Date of Birth</small>
-                    {/* <h3 className="font-semibold">{props.user.dateOfBirth}</h3> */}
+                    <h3 className="font-semibold">{props.user.dateOfBirth}</h3>
                   </div>
                   <div className="mb-8">
                     <small>Hospital Number</small>
-                    {/* <h3 className="font-semibold">{props.user.userNumber}</h3> */}
+                    <h3 className="font-semibold">{props.user.userNumber}</h3>
                   </div>
                   <div className="mb-8">
                     <small>Address</small>
-                    {/* <h3 className="font-semibold">{props.user.address}</h3> */}
+                    <h3 className="font-semibold">{props.user.address}</h3>
                   </div>
                   <div className="mb-8">
                     <small>Marital Status</small>
@@ -47,19 +47,21 @@ const PatientDetails = ( props) => {
                 <div className="main_one flex flex-1 flex-col text-right">
                   <div className="mb-8">
                     <small>Phone</small>
-                    {/* <h3 className="font-semibold">{props.user.phoneNumber}</h3> */}
+                    <h3 className="font-semibold">{props.user.phoneNumber}</h3>
                   </div>
                   <div className="mb-8">
                     <small>Hospital Number</small>
-                    {/* <h3 className="font-semibold">{props.user.gender}</h3> */}
+                    <h3 className="font-semibold">{props.user.gender}</h3>
                   </div>
                   <div className="mb-8">
                     <small>E-mail</small>
-                    {/* <h3 className="font-semibold">{props.user.email}</h3> */}
+                    <h3 className="font-semibold">{props.user.email}</h3>
                   </div>
                   <div className="mb-8">
-                    {/* <small>Marital Status</small>
-                <h3 className="font-semibold">{props.user.maritalStatus}</h3> */}
+                    <small>Marital Status</small>
+                    <h3 className="font-semibold">
+                      {props.user.maritalStatus}
+                    </h3>
                   </div>
                 </div>
               </div>
@@ -74,13 +76,13 @@ const PatientDetails = ( props) => {
               <div className="mt-8">
                 <small>Date</small>
                 <h3 className="font-semibold">
-                  {/* {props.user.appointment.appointmentTime} 21th April, 2021 */}
+                  {props.user.appointment[0].appointmentTime} 
                 </h3>
               </div>
-              <div className="mt-8">
+              <div className="mt-8 text-right">
                 <small>Time</small>
                 <h3 className="font-semibold">
-                  {/* {props.user.appointment[0].appointmentTime} */} 10am
+                  {props.user.appointment[0].appointmentDate} 
                 </h3>
               </div>
             </div>
@@ -88,20 +90,22 @@ const PatientDetails = ( props) => {
               <div className="mt-8">
                 <small>Concern</small>
                 <h3 className="font-semibold">
-                  {/* {props.user.appointment[0].concern} */} fever
+                  {props.user.appointment[0].concern} 
                 </h3>
               </div>
-              <div className="mt-8">
+              <div className="mt-8 text-right">
                 <small>Appointment Number</small>
                 <h3 className="font-semibold">
-                  {/* {props.user.appointment[0].appointmentNumber} */}
-                  appointment2932
+                  {props.user.appointment[0].appointmentNumber}
                 </h3>
               </div>
             </div>
           </div>
           {/* vitals */}
-          <EnterVitals userId={props.user._id}/>
+          <EnterVitals userId={props.user._id} />
+        </div>
+        <div style={{marginLeft:"450px", marginTop:"-200px"}}>
+          <Diagnosis userId= {props.user._id} />
         </div>
       </div>
       {/* </MainLayout> */}
@@ -136,7 +140,7 @@ export async function getStaticProps({ params }) {
 
   const user = data;
 
-  console.log(user)
+  // console.log(user)
 
   // Pass post data to the page via props
   return { props: { user } };
