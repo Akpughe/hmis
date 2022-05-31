0c629d19fc94a781b07307b07f0a0c255e075082
“Akpughe” <davidakpughe2@gmail.com>
2022-05-31 22:50:32 +0200

    [ADD] added image

--- frontendv2/pages/index.js
+++ frontendv2/pages/index.js
@@ -56,11 +56,11 @@ export default function Home() {
       <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8 shadow-lg bg-white p-5 py-10">
           <div>
-            {/* <img
+            <img
               className="mx-auto h-12 w-auto"
               src="/hpro.png"
               alt="Workflow"
-            /> */}
+            />
             <div></div>
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
               Sign in to your account
