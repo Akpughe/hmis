8016f6ab932a757f4287bc60c18e355b5f97ccc9
“Akpughe” <davidakpughe2@gmail.com>
2022-06-01 15:36:16 +0200

    [ADD] add patient to store

--- frontendv2/app/store.js
+++ frontendv2/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
+import patientReducer from '../features/patient/patientSlice';
 
 export function makeStore() {
   return configureStore({
     reducer: {
       auth: authReducer,
+      patients: patientReducer,
     },
   });
 }
