b3ad38ec0672bd27778a680e28e0f14ec94784f2
“Akpughe” <davidakpughe2@gmail.com>
2022-06-01 15:46:50 +0200

    [ADD] route for total number of patients

--- backend/routes/patient.js
+++ backend/routes/patient.js
@@ -9,6 +9,8 @@ const router = express.Router();
 
 router.get('/get-all-patients', patientController.getAllPatients);
 
+router.get('/get-total-patients', patientController.getTotalPatients);
+
 router.get('/', isAuth ,patientController.getPatientById)
 
 router.get('/:patientId', patientController.getPatientsById);
