6f9b00122bfb8991bb3dc7d89da2c33e4e95ee89
“Akpughe” <davidakpughe2@gmail.com>
2022-08-27 04:00:53 +0200

    api url

--- features/doctor/doctorService.ts
+++ features/doctor/doctorService.ts
@@ -1,6 +1,6 @@
 import { getAsync } from "../../lib/adapter";
-const API_URL = 'http://localhost:4000/api';
-// const API_URL = 'https://myhmis.herokuapp.com/api';
+// const API_URL = 'http://localhost:4000/api';
+const API_URL = 'https://myhmis.herokuapp.com/api';
 
 export const getDoctors = async () => {
   const response = await getAsync(API_URL + '/user/get-all-docs');
