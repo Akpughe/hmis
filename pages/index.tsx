2542f22c359b8ecf8b0e2d1844dfdb06ed62f10d
“Akpughe” <davidakpughe2@gmail.com>
2022-09-02 02:54:32 +0200

    add - new login screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,10 @@ import { useState, useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
 import { useRouter } from 'next/router';
 import { login, reset } from '../features/auth/authSlice';
-import {toast} from 'react-toastify';
-
+import { toast } from 'react-toastify';
 export interface LoginProps {
-  userNumber:string;
-  password:string;
+  userNumber: string;
+  password: string;
 }
 
 export default function Home() {
@@ -21,24 +20,24 @@ export default function Home() {
   const dispatch = useAppDispatch();
   const router = useRouter();
 
-  const {user, isLoading, isError, isSuccess, message } = useAppSelector(
+  const { user, isLoading, isError, isSuccess, message } = useAppSelector(
     (state) => state.auth
   );
 
-  useEffect(()=>{
-    if(isError){
+  useEffect(() => {
+    if (isError) {
       toast.error(message);
     }
 
-    if(isSuccess || user){
+    if (isSuccess || user) {
       router.push('/dashboard');
     }
 
     dispatch(reset());
-  },[user, isError, isSuccess, message, router, dispatch]);
+  }, [user, isError, isSuccess, message, router, dispatch]);
 
-  if(isLoading){
-    return <div>Loading...</div>
+  if (isLoading) {
+    return <div className="flex justify-center items-center w-full h-screen">Loading...</div>;
   }
 
   const onChange = (e) =>
@@ -48,8 +47,8 @@ export default function Home() {
     e.preventDefault();
     const userData = {
       userNumber,
-      password
-    }
+      password,
+    };
     dispatch(login(userData));
   };
   return (
@@ -58,25 +57,25 @@ export default function Home() {
         <title>Login</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-md w-full space-y-8 shadow-lg bg-white p-5 py-10">
-          <div>
+      <div className="m-auto max-w-md my-10">
+        <div className="flex flex-col  bg-white p-6">
+          {/* image */}
+          <div className="flex justify-center mt-8">
             <img
-              className="mx-auto h-12 w-auto"
-              src="/hpro.png"
-              alt="Workflow"
+              src={`https://avatars.dicebear.com/api/avataaars/${Date.now()}.svg`}
+              className="w-20 h-20 rounded full"
+              alt=""
             />
-            <div></div>
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              Sign in to your account
-            </h2>
           </div>
-          <form className="mt-8 space-y-6" onSubmit={(e) => onSubmit(e)}>
-            <input type="hidden" name="remember" value="true" />
-            <div className="rounded-md shadow-sm -space-y-px">
+          <div className="mt-10">
+            {/* form here */}
+            <form className="" onSubmit={(e) => onSubmit(e)}>
               <div>
-                <label htmlFor="staffNumber" className="sr-only">
-                  User Number
+                <label
+                  className="font-bold text-[#3A4374] text-sm"
+                  htmlFor="title"
+                >
+                  User number
                 </label>
                 <input
                   name="userNumber"
@@ -84,12 +83,13 @@ export default function Home() {
                   value={userNumber}
                   onChange={(e) => onChange(e)}
                   required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="User Number"
                 />
               </div>
-              <div>
-                <label htmlFor="password" className="sr-only">
+              <div className="mt-4">
+                <label
+                  className="font-bold text-[#3A4374] text-sm"
+                  htmlFor="title"
+                >
                   Password
                 </label>
                 <input
@@ -97,66 +97,22 @@ export default function Home() {
                   type="password"
                   value={password}
                   onChange={(e) => onChange(e)}
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Password"
+                  placeholder=""
                 />
               </div>
-            </div>
-
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                <input
-                  id="remember_me"
-                  name="remember_me"
-                  type="checkbox"
-                  className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                />
-                <label
-                  htmlFor="remember_me"
-                  className="ml-2 block text-sm text-gray-900"
+              <div className="mt-8">
+                <button
+                  className="flex items-center justify-center w-full font-bold text-xs text-white rounded-lg px-8 h-11 bg-[#ad20ea]"
+                  type="submit"
                 >
-                  Remember me
-                </label>
+                  Login
+                </button>
               </div>
-
-              <div className="text-sm">
-                <a
-                  href="#"
-                  className="font-medium text-indigo-600 hover:text-indigo-500"
-                >
-                  Forgot your password?
-                </a>
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                  {/* <!-- Heroicon name: lock-closed --> */}
-                  <svg
-                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </span>
-                Sign in
-              </button>
-            </div>
-          </form>
+            </form>
+            {/* end form */}
+          </div>
         </div>
-      </div>
+      </div>{' '}
     </>
   );
 }
